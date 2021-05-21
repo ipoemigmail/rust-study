db8a5fe4db26eed4c9722538256763785b81ea64
ben.jung <ben.jung@kakaocommerce.com>
2021-05-21 16:52:46 +0900

    async-zookeeper-exam added

---
+++ async-zookeeper-exam/src/main.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use futures::{future::join_all, stream::{self, StreamExt}};
+use log::info;
+use std::{rc::Rc, str, sync::Arc, time};
+
+use zookeeper_async::{WatchedEvent, Watcher, ZooKeeper};
+struct LoggingWatcher;
+impl Watcher for LoggingWatcher {
+    fn handle(&self, e: WatchedEvent) {
+        println!("{:?}", e)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let zk_client = Arc::new(ZooKeeper::connect("localhost:2181", time::Duration::from_secs(1), LoggingWatcher).await.unwrap());
+
+    //let client = ZooKeeper::connect("localhost:2181", time::Duration::from_secs(1), LoggingWatcher).unwrap();
+    let path = "key1";
+    let tasks = (0..100000).map(|_| {
+        let s = zk_client.clone();
+        tokio::spawn(async move { s.get_data("/kakao/commerce/kcdl/v2/jobs/meta/dev-hadoop/local/test.app/config/key1", false).await })
+    }).collect::<Vec<_>>();
+    let a = stream::iter(tasks).then(|f| async move { f.await }).collect::<Vec<_>>().await;
+    //let a = join_all(tasks).await;
+    println!("{:?}", a[0]);
+    //println!("{:?}", data);
+}

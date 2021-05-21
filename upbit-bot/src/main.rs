915fe93a414613b79936a66378e9bec7fde79bef
ben.jung <ben.jung@kakaocommerce.com>
2021-05-22 01:38:10 +0900

    upbit-bot change

--- upbit-bot/src/main.rs
+++ upbit-bot/src/main.rs
@@ -1,5 +1,6 @@
 mod upbit;
 
+use static_init::dynamic;
 use anyhow::Result;
 use chrono::Local;
 use futures::stream;
@@ -12,18 +13,19 @@ use std::{
 };
 use upbit::*;
 
-const BUFFER_SIZE: usize = 60 * 10;
-const DETECTED_RATE: f64 = 0.05;
+#[dynamic]
+static BUFFER_SIZE: usize = 60 * 10;
+#[dynamic]
+static DETECTED_RATE: Decimal = Decimal::from_f64(0.05).unwrap();
 
 fn check(xs: &Vec<MarketTicker>) {
-    //print!("{}, ", xs.first().unwrap().market);
-    if xs.len() >= BUFFER_SIZE {
+    if xs.len() >= *BUFFER_SIZE {
         let mut ys = xs.clone();
         ys.sort_by(|x, y| x.trade_price.partial_cmp(&y.trade_price).unwrap());
         let last = xs.last().unwrap();
         let min = xs.first().unwrap();
         let diff = last.trade_price.clone() - min.trade_price.clone();
-        if last.trade_price.clone() * Decimal::from_f64(DETECTED_RATE.into()).unwrap() < diff {
+        if last.trade_price.clone() * *DETECTED_RATE < diff {
             let dt = Local::now();
             println!(
                 "[{}] {}, last: {}, min: {}",
@@ -40,7 +42,7 @@ fn check(xs: &Vec<MarketTicker>) {
 async fn main() -> Result<()> {
     let upbit_service = Arc::new(UpbitServiceLive::new());
     let mut interval = tokio::time::interval(Duration::from_secs(1));
-    let mut buffer: HashMap<String, Arc<Mutex<Vec<MarketTicker>>>> = HashMap::new();
+    let mut market_ticker_buffer: HashMap<String, Arc<Mutex<Vec<MarketTicker>>>> = HashMap::new();
     loop {
         interval.tick().await;
         let s = upbit_service.market_list().await?;
@@ -52,19 +54,19 @@ async fn main() -> Result<()> {
         let market_tickers = upbit_service.market_ticker_list(ids).await?;
         for market_ticker in market_tickers {
             let key = market_ticker.market.as_str();
-            if let None = buffer.get(market_ticker.market.as_str()) {
-                buffer.insert(
+            if let None = market_ticker_buffer.get(market_ticker.market.as_str()) {
+                market_ticker_buffer.insert(
                     key.to_owned(),
-                    Arc::new(Mutex::new(Vec::with_capacity(BUFFER_SIZE))),
+                    Arc::new(Mutex::new(Vec::with_capacity(BUFFER_SIZE.to_be()))),
                 );
             }
-            let mut b = buffer.get_mut(key).unwrap().lock().unwrap();
-            if b.len() > BUFFER_SIZE {
+            let mut b = market_ticker_buffer.get_mut(key).unwrap().lock().unwrap();
+            if b.len() > BUFFER_SIZE.to_be() {
                 b.pop();
             }
             b.insert(0, market_ticker);
         }
-        let tasks: Vec<_> = buffer
+        let tasks: Vec<_> = market_ticker_buffer
             .iter()
             .map(move |(_, v)| {
                 let vv = v.clone();
